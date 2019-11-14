8fa4ffa9aac2424ed409423d7be51e9c4140ea15
wldnjsl216 <brandnew7th@gmail.com>
2019-11-14 10:37:30 -0500

    fixed pim_id missing issue

--- SMC/GEM5/utils/python/ethz_pimconf.py
+++ SMC/GEM5/utils/python/ethz_pimconf.py
@@ -33,7 +33,7 @@ def makeStandalonePIMSystem(mem_mode, num_cpus = 1):
 	return self
 
 ##############################################################################
-def build_pim_system(options, mem_mode, cpu_class):
+def build_pim_system(options, mem_mode, cpu_class, pim_id=0): # JIWON: added pim_id parameter
 	
     pim_sys = makeStandalonePIMSystem(mem_mode)
 
@@ -62,9 +62,9 @@ def build_pim_system(options, mem_mode, cpu_class):
     # ethz_print_val("Host Class", cpu_class);
     # ethz_print_val("PIM  Class", type(pim_sys.cpu));
 
-    pim_sys.itlb = ethz_TLB()
-    pim_sys.dtlb = ethz_TLB()
-    pim_sys.stlb = ethz_TLB()
+    pim_sys.itlb = ethz_TLB(pim_id=pim_id)
+    pim_sys.dtlb = ethz_TLB(pim_id=pim_id)
+    pim_sys.stlb = ethz_TLB(pim_id=pim_id)
 
     pim_sys.dma = ethz_DMA()
     pim_sys.dma.dma = pim_sys.pimbus.slave
@@ -98,7 +98,9 @@ def build_pim_system(options, mem_mode, cpu_class):
     pim_sys.cpu.icache_port = pim_sys.itlb.slave
 
     pim_sys.itlb.master = pim_sys.pimbus.slave
-    # Default ranges for the TLBs
+
+    # JIWON: commented out original code and added modified code below
+    '''# Default ranges for the TLBs
     pim_sys.itlb.original_ranges = AddrRange(0x00000000, size=PIM_ADDRESS_SIZE)
     pim_sys.dtlb.original_ranges = AddrRange(0x00000000, size=PIM_ADDRESS_SIZE)
     pim_sys.itlb.remapped_ranges = AddrRange(PIM_ADDRESS_BASE, size=PIM_ADDRESS_SIZE)
@@ -166,7 +168,79 @@ def build_pim_system(options, mem_mode, cpu_class):
     pim_sys.machine_type = options.machine_type    # Erfan
 
     # pim_sys.realview.nvmem = SimpleMemory(range = AddrRange(0, size = '1MB'))
-    # pim_sys.realview.nvmem.port = test_sys.membus.master
+    # pim_sys.realview.nvmem.port = test_sys.membus.master'''
+    pim_address_offset = pim_id * PIM_ADDRESS_SIZE_INT
+    pim_base_virtual_addr = 0x00000000 #+ pim_address_offset  # JIWON TODO: pim offset? needed? or not?
+    pim_base_physical_addr = PIM_ADDRESS_BASE + pim_address_offset 
+
+    # Default ranges for the TLBs
+    pim_sys.itlb.original_ranges = AddrRange(pim_base_virtual_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.dtlb.original_ranges = AddrRange(pim_base_virtual_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.itlb.remapped_ranges = AddrRange(pim_base_physical_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.dtlb.remapped_ranges = AddrRange(pim_base_physical_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.dtlb.OS_PAGE_SHIFT = OS_PAGE_SHIFT   # Page shift of the guest OS
+    pim_sys.dtlb.TLB_SIZE = PIM_DTLB_SIZE
+    pim_sys.dtlb.DUMP_ADDRESS = True if (PIM_DTLB_DUMP_ADDRESS=="TRUE") else False
+    pim_sys.dtlb.IDEAL_REFILL = True if (PIM_DTLB_DO_IDEAL_REFILL=="TRUE") else False
+
+    # Pointer to the slice table
+    pim_sys.stlb.original_ranges = AddrRange(pim_base_virtual_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.stlb.remapped_ranges = AddrRange(pim_base_physical_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.system_port = pim_sys.stlb.slave
+    pim_sys.stlb.master = pim_sys.pimbus.slave
+        
+    r = AddrRange(pim_base_physical_addr, size=PIM_ADDRESS_SIZE)
+    pim_sys.pim_memory = ethz_PIMMemory(range=r, latency=PIM_SPM_ACCESSTIME_ns, bandwidth=PIM_SPM_BW_Gbps, pim_id=pim_id ) # JIWON: added pim_id
+    pim_sys.pim_memory.PIM_ADDRESS_BASE = pim_base_physical_addr 
+    pim_sys.pim_memory.PIM_DEBUG_ADDR = PIM_DEBUG_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_ERROR_ADDR = PIM_ERROR_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_INTR_ADDR = PIM_INTR_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_COMMAND_ADDR = PIM_COMMAND_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_STATUS_ADDR = PIM_STATUS_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_SLICETABLE_ADDR = PIM_SLICETABLE_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_SLICECOUNT_ADDR = PIM_SLICECOUNT_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_SLICEVSTART_ADDR = PIM_SLICEVSTART_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_M5_ADDR = PIM_M5_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_M5_D1_ADDR = PIM_M5_D1_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_M5_D2_ADDR = PIM_M5_D2_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_VREG_ADDR = PIM_VREG_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_VREG_SIZE = PIM_VREG_SIZE
+    pim_sys.pim_memory.PIM_SREG_ADDR = PIM_SREG_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_SREG_COUNT = PIM_SREG_COUNT
+    pim_sys.pim_memory.PIM_DMA_MEM_ADDR_ADDR = PIM_DMA_MEM_ADDR_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_DMA_SPM_ADDR_ADDR = PIM_DMA_SPM_ADDR_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_DMA_NUMBYTES_ADDR = PIM_DMA_NUMBYTES_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_DMA_COMMAND_ADDR = PIM_DMA_COMMAND_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_DMA_CLI_ADDR = PIM_DMA_CLI_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_DTLB_IDEAL_REFILL_ADDR = PIM_DTLB_IDEAL_REFILL_ADDR + pim_address_offset
+    pim_sys.pim_memory.HMC_ATOMIC_INCR_ADDR = HMC_ATOMIC_INCR_ADDR + pim_address_offset
+    pim_sys.pim_memory.PIM_COPROCESSOR_CMD_ADDR = PIM_COPROCESSOR_CMD_ADDR + pim_address_offset
+    pim_sys.dtlb.PIM_DTLB_IDEAL_REFILL_REG = PIM_DTLB_IDEAL_REFILL_REG # + pim_address_offset # JIWON: PIM virtual address 
+    pim_sys.dtlb.HMC_ATOMIC_INCR = HMC_ATOMIC_INCR #+ pim_address_offset # JIWON: PIM virtual address
+    pim_sys.dtlb.HMC_ATOMIC_IMIN = HMC_ATOMIC_IMIN #+ pim_address_offset  # JIWON: PIM virtual address
+    pim_sys.dtlb.HMC_ATOMIC_FADD = HMC_ATOMIC_FADD #+ pim_address_offset  # JIWON: PIM virtual address
+    pim_sys.dtlb.HMC_OPERAND = HMC_OPERAND #+ pim_address_offset # JIWON: PIM virtual address
+    pim_sys.pim_memory.SYSTEM_NUM_CPU = GEM5_NUMCPU
+    pim_sys.pim_memory.port = pim_sys.pimbus.default
+    pim_sys.kernel_addr_check = False;		# Do not check for correct location of kernel (we do address translation in TLB)
+
+    # Specify the architecture in the components
+    if ( ARCH=="arm64"):
+        pim_sys.pim_memory.pim_arch = 64
+        pim_sys.dtlb.pim_arch = 64
+    elif (ARCH=="arm"):
+        pim_sys.pim_memory.pim_arch = 32
+        pim_sys.dtlb.pim_arch = 32
+    else:
+        raise ValueError('ARCH is incorrect!');
+
+    # Necessary options (specially for ARMv8)
+    pim_sys.boot_loader = pim_sys.kernel
+    pim_sys.gic_cpu_addr = 0x00000001 # Dummy
+    pim_sys.flags_addr = 0x00000001 # Dummy
+    pim_sys.machine_type = options.machine_type    # Erfan
+
+    # end of JIWON's modifications
 
 
     return pim_sys
